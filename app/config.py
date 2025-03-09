d47940762f4114a808e4297de3f498ea3e0b473f
Joan Fabrégat <j@fabreg.at>
2025-03-09 19:16:52 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = os.getenv("VERSION", "0.1.0")
-BUILD_ID = os.getenv("BUILD_ID", "unknown")
-COMMIT_SHA = os.getenv("COMMIT_SHA", "unknown")
-PORT = os.getenv("PORT", 8000)
+VERSION = os.getenv("VERSION") or "unknown"
+BUILD_ID = os.getenv("BUILD_ID") or "unknown"
+COMMIT_SHA = os.getenv("COMMIT_SHA") or "unknown"
+PORT = os.getenv("PORT") or 8000
